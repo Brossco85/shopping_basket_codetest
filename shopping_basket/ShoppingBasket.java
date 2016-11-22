9ff88333f50f2d9c312683103f22ac850fddd1d4
Bobby Ross <bobby1985uk@gmail.com>
2016-11-22 20:26:53 +0000

    shopping basket getBasketSize method added and tested

--- shopping_basket/ShoppingBasket.java
+++ shopping_basket/ShoppingBasket.java
@@ -1,2 +1,20 @@
 package shopping_basket;
 import java.util.*;
+
+public class ShoppingBasket{
+
+ArrayList<Item> basket = new ArrayList<Item>();
+
+public ShoppingBasket(){
+}
+
+public ArrayList<Item> getBasket(){
+  return this.basket;
+}
+
+public int getBasketSize(){
+  return basket.size();
+}
+
+
+}
---
+++ shopping_basket_specs/ShoppingBasketTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+import shopping_basket.*;
+
+public class ShoppingBasketTest{
+
+ShoppingBasket basket1;
+Item item1;
+Item item2;
+Item item3;
+
+@Before
+public void before(){
+  basket1 = new ShoppingBasket();
+  item1 = new Item("Apple", "Iphone", 300);
+  item2 = new Item("Apple", "Macbook", 500);
+  item3 = new Item("MicroSoft", "SurfacePro", 700);
+}
+
+@Test
+public void testStartBasketEmpty(){
+  assertEquals(0, basket1.getBasketSize());
+}
+
+
+}
